3c5838779f2f54b93343135718ef98a28f45ef60
unknown <a99701841@gmail.com>
2026-02-10 13:53:42 +0900

    update

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { communityPosts } from '../data/board';
+import './styles/Board.css'
 
 const Board = () => {
   return (
-    <section>Board</section>
+    <section>
+      <div className='inner'>
+        <div className='card-lst'>
+          {communityPosts.map(({id, title, content})=>(
+          <div key={id} className='card'>
+            <h4>{title}</h4>
+            <p>{content}</p>
+          </div>
+        ))}
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
-import './styles/Home.css'
-
+import "./styles/Home.css"
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import { Pagination ,Navigation,Autoplay } from 'swiper/modules';
+import { Pagination, Navigation, Autoplay } from 'swiper/modules';
+import { homeSlides } from '../data/homeSlide';
 const Home = () => {
   return (
-    <section>
+    <section className='home'>
       <div className="inner">
 
         <h2>home</h2>
@@ -17,12 +17,22 @@ const Home = () => {
           navigation={true}
           // autoplay={true}
           loop
-          modules={[Navigation, Pagination ,Autoplay]}
+          modules={[Navigation, Pagination, Autoplay]}
           pagination={true}
-          className="mySwiper">
-          <SwiperSlide>Slide 1</SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
+          className="home-slider">
+          {homeSlides.map(({ id, title, img, subtitle }) => (
+
+            <SwiperSlide key={id} style={{ backgroundImage: `url(${img})` }}>
+              <div className="inner">
+
+
+                <h4>
+                  {title}
+                </h4>
+                <p>{subtitle}</p>
+              </div>
+            </SwiperSlide>
+          ))}
 
         </Swiper>
       </div>
